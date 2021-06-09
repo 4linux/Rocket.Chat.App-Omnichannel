b03615c51557d4e15904d0539660245c8be74d80
Caique Portela <caiquesportela@gmail.com>
2021-06-09 12:22:35 -0300

    Implementado funcionalidades inicialmente desejadas

---
+++ README.md
@@ -0,0 +1,16 @@
+# Rocket.Chat.App-Omnichannel
+
+Aplicação com o objetivo de adicionar funcionalidades extras ao Omnichannel.
+
+Atualmente permite:
+
+- Adicionar nome do agente antes de suas mensagens (apenas na primeira de uma sequência)
+- Informar ao usuário quando ele é transferido
+- Informar qual agente se conectou a sala
+   
+---
+
+Criada a partir de uma série de posts para o [blog da 4Linux](https://blog.4linux.com.br/) que explicam as funcionalidades dos aplicativos para o rocketchat.
+
+- https://blog.4linux.com.br/rocket-chat-app-adicione-funcionalidades
+- https://blog.4linux.com.br/rocket-chat-app-criando-nosso-primeiro-aplicativo
--- RocketChatAppOmnichannelApp.ts
+++ RocketChatAppOmnichannelApp.ts
@@ -1,12 +1,98 @@
 import {
     IAppAccessors,
+    IHttp,
     ILogger,
+    IMessageBuilder,
+    IModify,
+    IPersistence,
+    IRead,
 } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { ILivechatEventContext, ILivechatRoom, ILivechatTransferEventContext, IPostLivechatAgentAssigned, IPostLivechatRoomTransferred, LivechatTransferEventType } from '@rocket.chat/apps-engine/definition/livechat';
+import { IMessage, IPreMessageSentModify } from '@rocket.chat/apps-engine/definition/messages';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { RoomType } from '@rocket.chat/apps-engine/definition/rooms';
+import { IUser, UserType } from '@rocket.chat/apps-engine/definition/users';
+export class RocketChatAppOmnichannelApp extends App implements IPreMessageSentModify, IPostLivechatRoomTransferred, IPostLivechatAgentAssigned {
 
-export class RocketChatAppOmnichannelApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
+
+    public async executePostLivechatAgentAssigned(
+        context: ILivechatEventContext,
+        read: IRead,
+        http: IHttp,
+        persis: IPersistence,
+        modify: IModify,
+    ): Promise<void> {
+        if (context.room.type !== RoomType.LIVE_CHAT) {
+            return;
+        }
+
+        const appUser = await read.getUserReader().getAppUser() as IUser;
+
+        const message = modify.getCreator()
+            .startLivechatMessage()
+            .setRoom(context.room)
+            .setText(`Você está falando com o atendente ${context.agent.name}.`)
+            .setSender(appUser);
+
+        await modify.getCreator().finish(message);
+    }
+
+    public async executePostLivechatRoomTransferred(
+        context: ILivechatTransferEventContext,
+        read: IRead,
+        http: IHttp,
+        persis: IPersistence,
+        modify: IModify,
+    ): Promise<void> {
+        if (context.room.type !== RoomType.LIVE_CHAT) {
+            return;
+        }
+
+        const messageText = context.type === LivechatTransferEventType.AGENT ?
+            `*Você foi transferido um outro atendente.*` :
+            `*Você foi transferido para o departamento ${context.to.name}.*`;
+
+        const appUser = await read.getUserReader().getAppUser() as IUser;
+
+        const message = modify.getCreator()
+            .startLivechatMessage()
+            .setRoom(context.room)
+            .setText(messageText)
+            .setSender(appUser);
+
+        await modify.getCreator().finish(message);
+    }
+
+    public async executePreMessageSentModify(
+        message: IMessage,
+        builder: IMessageBuilder,
+        read: IRead,
+        http: IHttp,
+        persistence: IPersistence,
+    ): Promise<IMessage> {
+        if (message.room.type !== RoomType.LIVE_CHAT) {
+            return await builder.getMessage();
+        }
+
+        if (message.sender.type !== UserType.USER) {
+            return await builder.getMessage();
+        }
+
+        const room = message.room as ILivechatRoom;
+
+        if (!room.isWaitingResponse) {
+            return await builder.getMessage();
+        }
+
+        const messageText = `*${message.sender.name}:*\n\n\n${message.text}`;
+
+        builder.setText(messageText);
+
+        return await builder.getMessage();
+    }
+
 }
